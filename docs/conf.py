cd5fcc0385e7745c5a3438aaff5688d8d6ef40c2
jhmatthews <matthews@ast.cam.ac.uk>
2022-01-12 15:58:49 +0000

    added logo

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,8 @@ pygments_style = 'sphinx'
 #
 # html_theme = 'alabaster'
 html_theme = "sphinx_book_theme"
+html_logo = "source/img/logo.png"
+html_favicon = "source/img/logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -103,6 +105,7 @@ html_theme = "sphinx_book_theme"
 html_theme_options = {
     "repository_url": "https://github.com/jhmatthews/alpro",
     "use_repository_button": True,
+    "logo_only": True,
 }
 
 
