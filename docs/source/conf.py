fc1e202d0f0afa0795d60058b22019f0c6630f2f
jhmatthews <matthews@ast.cam.ac.uk>
2022-02-17 10:22:12 +0000

    will this make sphinx import work?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 # import os
 import sys
-sys.path.insert(0, '../../alpro/')
+sys.path.insert(0, '../../')
 import alpro
 
 # -- Project information -----------------------------------------------------
