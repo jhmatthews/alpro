3334f6a007c2bce4b4992557d868a0f31844006f
jhmatthews <matthews@ast.cam.ac.uk>
2022-02-18 10:07:23 +0000

    mods to README and setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,6 @@ from distutils.core import setup, Extension
 import numpy
 import os
 
-#os.environ["CC"] = "gcc-10"
-libraries = ['m', 'gsl', 'gslcblas']
-
-# IMPROVE this should be a symbolic link / or an env variable
-library_dirs = ['gsl_lib']
-include_dir = 'gsl_include'
-#os.environ["CFLAGS"] = "-O0"
-
-# define the extension module
-# alpro_module = Extension('alprocore',
-#                     include_dirs=[numpy.get_include(), include_dir],
-#                     libraries = libraries,
-#                     library_dirs = library_dirs,
-#                     sources = ['alpro/core/alpro.c', 'alpro/core/alpro_matrix.c'])
-
 # data files 
 data_files = ["data/*.dat"]
 
